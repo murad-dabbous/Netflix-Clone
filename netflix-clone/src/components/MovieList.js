855fb2eec9751294f2d6fa8ba8673133c99460c0
murad-dabbous <moradyousef04@gmail.com>
2023-05-24 14:48:40 +0000

    Fixed the issues that i had in the first submission

--- netflix-clone/src/components/MovieList.js
+++ netflix-clone/src/components/MovieList.js
@@ -1,6 +1,10 @@
 import Movie from "./Movie";
 
-const MovieList = ({ data, setSingleMovie }) => {
-  return <Movie data={data} setSingleMovie={setSingleMovie} />;
-};
+const MovieList = ({ data, setSingleMovie }) =>
+  // return <Movie data={data} setSingleMovie={setSingleMovie} />;
+
+  data.length &&
+  data.map((dataItem) => (
+    <Movie key={dataItem.id} data={dataItem} setSingleMovie={setSingleMovie} />
+  ));
 export default MovieList;
